44d2b6900bcc3551aabb6fcffea1f861a3a8ab62
Thomas Swift <tswift@u.rochester.edu>
2013-10-30 21:47:06 -0400

    broke runPlayers() into more modular pieces and moved these helper function calls into main

--- src/football/FantasyFootballCustomScoringHelper.java
+++ src/football/FantasyFootballCustomScoringHelper.java
@@ -16,12 +16,7 @@ import football.util.metrics.SortOrderMetric;
 
 public class FantasyFootballCustomScoringHelper
 {
-	//files to write results to
-	public static final String resultsDir = "results";
-	public static final String sectionDenoter = "********";
-	public static final String delimiter = "***********************************************************";
-
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		if(args.length < 1) {
 			System.out.println("Error in main: Mode not specified");
@@ -53,48 +48,22 @@ public class FantasyFootballCustomScoringHelper
 			System.exit(1);
 		}
 
-		String filename = (mode.toUpperCase() + "results.txt");
+		//put players into list
 		List<Player> defaultPlayers = new ArrayList<Player>(Arrays.asList(players));
-		List<Player> customPlayers = runPlayers(defaultPlayers,args,filename);
-	}
-
-	//TODO: replace List<Player> with List<T extends Player>
-	//could pass in PrintStream instead of filename string to make switching to System.out. easy
-	public static List<Player> runPlayers(List<Player> defaultPlayers, String[] args, String filename) {
-		//sort players according to default scores
-		Collections.sort(defaultPlayers);
 		//make copy of players to preserve original order
-		List<Player> customPlayers = new ArrayList<Player>(defaultPlayers.size());
-		for(Player player : defaultPlayers) {
-			customPlayers.add(player.deepCopy());
-		}
+		List<Player> customPlayers = deepCopyList(defaultPlayers);
 		//evaluate all players with custom rules
 		for(Player player : customPlayers) {
 			player.parseScoringCoeffsAndEvaluate(args);
 		}
-		//sort players according to custom scores
+		//sort players according to default rules
+		Collections.sort(defaultPlayers);
+		//sort players according to custom rules
 		Collections.sort(customPlayers);
-		//write results to file
-		try {
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File(resultsDir,filename),true)));
-			out.println(delimiter + "\n");
-			out.println(toSectionHeader("Custom scoring coefficients"));
-			out.println(customPlayers.get(0).categoriesToString()); //TODO: make this call static
-			printCoeffs(args,out);
-			out.println(toSectionHeader("Default scoring rules"));
-			printList(defaultPlayers,out);
-			out.println(toSectionHeader("Custom scoring rules"));
-			printList(customPlayers,out);
-			//calculate (dis)similarity btw customPlayers and defaultPlayers
-			Metric metric = new SortOrderMetric();
-			double dist = metric.distance(defaultPlayers,customPlayers);
-			out.println("Distance between default and custom is: " + dist);
-			out.println("\n" + delimiter + "\n\n\n");
-			out.close();
-		} catch(IOException e) {
-			System.out.println("Error in runPlayers: IO exception when writing to file " + filename);
-		}
-		return customPlayers;
+		//write results to file filename in directory resultsDir
+		String resultsDirectory = "results";
+		String filename = (mode.toUpperCase() + "results.txt");
+		logResults(resultsDirectory,filename,defaultPlayers,customPlayers,args);
 	}
 
 	private static void usage() {
@@ -109,6 +78,15 @@ public class FantasyFootballCustomScoringHelper
 		System.out.println(indent + "wr: " + WR.categoriesToString());*/
 	}
 
+	//Make deep copy of list of players. Works for any List<E> where E defines a deepCopy() method
+	private static List<Player> deepCopyList(List<Player> list) {
+		List<Player> listCopy = new ArrayList<Player>(list.size());
+		for(Player element : list) {
+			listCopy.add(element.deepCopy());
+		}
+		return listCopy;
+	}
+
 	//TODO: consider just using players.toString()
 	//write players list to printwriter stream
 	private static <E extends Player> void printList(List<E> players, PrintWriter out) {
@@ -116,10 +94,9 @@ public class FantasyFootballCustomScoringHelper
 			out.println(player.toString());
 		}
 		out.println("\n");
-		out.flush(); //for System.out casted to printwriter
+		//out.flush(); //for System.out casted to printwriter
 	}
 
-	//TODO: format this better for players with different stat types
 	//write coeffs array to printwriter stream
 	private static void printCoeffs(String[] args, PrintWriter out) {
 		int numCoeffs = args.length;
@@ -130,7 +107,28 @@ public class FantasyFootballCustomScoringHelper
 	}
 
 	//surrounds sectionTitle on both sides by sectionDenoter to create section header
-	private static String toSectionHeader(String sectionTitle) {
+	private static String toSectionHeader(String sectionTitle, String sectionDenoter) {
 		return (sectionDenoter + " " + sectionTitle + " " + sectionDenoter);
+	}
+
+	private static void logResults(String resultsDirectory, String filename, List<Player> defaultPlayers, 
+			List<Player> customPlayers, String[] args) throws IOException {
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File(resultsDirectory,filename),true)));
+		String delimiter = "***********************************************************";
+		String sectionDenoter = "********";
+		out.println(delimiter + "\n");
+		out.println(toSectionHeader("Custom scoring rules",sectionDenoter));
+		out.println(customPlayers.get(0).categoriesToString()); //TODO: make this call static
+		printCoeffs(args,out);
+		out.println(toSectionHeader("Scores using default rules",sectionDenoter));
+		printList(defaultPlayers,out);
+		out.println(toSectionHeader("Scores using custom rules",sectionDenoter));
+		printList(customPlayers,out);
+		//calculate (dis)similarity btw customPlayers and defaultPlayers
+		Metric metric = new SortOrderMetric();
+		double dist = metric.distance(defaultPlayers,customPlayers);
+		out.println("Distance between default and custom rules is: " + dist);
+		out.println("\n" + delimiter + "\n\n\n");
+		out.close();
 	}
 }
