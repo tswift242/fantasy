82fa49f598e963857fc97431930ceff7d2dc0a44
Thomas Swift <tswift@u.rochester.edu>
2014-08-16 15:23:51 -0400

    PlayersPanel now uses JTable instead of JLabels-GridLayout combo

--- src/main/java/football/core/graphics/PlayersPanel.java
+++ src/main/java/football/core/graphics/PlayersPanel.java
@@ -1,9 +1,11 @@
 package football.core.graphics;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.util.List;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+//import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
 import football.players.Player;
 
@@ -15,29 +17,87 @@ import football.players.Player;
 public final class PlayersPanel extends JPanel
 {
 	private static final long serialVersionUID = 7401650925506787631L;
-	private static final String WHITESPACE_REGEX = "\\s+";
 
 	public PlayersPanel(List<Player> players) {
-		int numPlayers = players.size();
-		// stat categories for this group of players
-		String[] categories = players.get(0).categoriesToString().split(WHITESPACE_REGEX);
-		int numStats = categories.length; // numStats == numCategories
-		//TODO: change layout from grid to gridbag to enable player name to be larger than stats?
-		this.setLayout(new GridLayout(numPlayers+1, numStats+2));
-		// add top row for stat categories
-		this.add(new JLabel("Players", JLabel.CENTER));
-		for(String category : categories) {
-			this.add(new JLabel(category, JLabel.CENTER));
-		}
-		this.add(new JLabel("Score", JLabel.CENTER));
-		// add new row of labels for each player, listing that player's name, stats, and score
-		for(Player player : players) {
-			this.add(new JLabel(player.getName(), JLabel.CENTER));
-			String[] stats = player.statsToString().split(WHITESPACE_REGEX);
-			for(String stat : stats) {
-				this.add(new JLabel(stat, JLabel.CENTER));
+		// construct table and add to panel
+		JTable table = new JTable(new PlayersTableModel(players));
+		this.setLayout(new BorderLayout());
+		this.add(table.getTableHeader(), BorderLayout.PAGE_START);
+		this.add(table, BorderLayout.CENTER);
+
+		//JScrollPane scrollPane = new JScrollPane(table);
+		//table.setFillsViewportHeight(true);
+		//this.add(scrollPane);
+	}
+
+
+
+	private class PlayersTableModel extends AbstractTableModel {
+		private static final String WHITESPACE_REGEX = "\\s+";
+
+		private String[] columnNames;
+		private Object[][] playerData;
+
+		public PlayersTableModel(List<Player> players) {
+			// stat categories for this group of players
+			String[] categories = players.get(0).categoriesToString().split(WHITESPACE_REGEX);
+			// create column names
+			int numStats = categories.length; // numStats == numCategories
+			int numCols = numStats+2;
+			columnNames = new String[numCols];
+			columnNames[0] = "Players";
+			System.arraycopy(categories, 0, columnNames, 1, numStats);
+			columnNames[numCols-1] = "Score";
+
+			// create row data
+			int numPlayers = players.size();
+			playerData = new Object[numPlayers][numCols];
+			int rowIdx = 0; // don't assume List has random access
+			for(Player player : players) {
+				playerData[rowIdx][0] = player.getName();
+				String[] statStrings = player.statsToString().split(WHITESPACE_REGEX);
+				// convert stats back from strings to ints
+				//TODO: see if we can avoid double converson by changing Player subclasses
+				Integer[] stats = toIntegerArray(statStrings);
+				System.arraycopy(stats, 0, playerData[rowIdx], 1, stats.length);
+				playerData[rowIdx++][numCols-1] = player.getScore();
 			}
-			this.add(new JLabel(Double.toString(player.getScore()), JLabel.CENTER));
+		}
+
+		@Override
+		public int getRowCount() {
+			return playerData.length;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public Object getValueAt(int row, int col) {
+			return playerData[row][col];
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+
+		@Override
+		public Class<?> getColumnClass(int col) {
+			return getValueAt(0, col).getClass();
+		}
+
+
+		// convert String[] to Integer[]
+		private Integer[] toIntegerArray(String[] array) {
+			int length = array.length;
+			Integer[] result = new Integer[length];
+			for(int i = 0; i < length; i++) {
+				result[i] = Integer.valueOf(array[i]);
+			}
+			return result;
 		}
 	}
 }
