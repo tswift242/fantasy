a30a28471146eef86aba3acf032105d4e8ac4555
Thomas Swift <tswift@u.rochester.edu>
2014-08-22 18:44:47 -0400

    now runs either GUI or cmdline version depending on if cmdline args are provided

--- src/main/java/football/FantasyFootballCustomScoringHelper.java
+++ src/main/java/football/FantasyFootballCustomScoringHelper.java
@@ -2,19 +2,30 @@ package football;
 
 import javax.swing.JFrame;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import football.core.CustomScoringHelperController;
 import football.core.CustomScoringHelperModel;
 import football.core.CustomScoringHelperView;
 
 public class FantasyFootballCustomScoringHelper
 {
+	private static final Logger logger = LoggerFactory.getLogger(FantasyFootballCustomScoringHelper.class.getName());
+
 	public static void main(String[] args)
 	{
 		CustomScoringHelperModel model = new CustomScoringHelperModel();
-		//TODO: feature flag here between running and creating View/Controller
-		//model.run(args);
-		CustomScoringHelperView view = new CustomScoringHelperView(model);
-		CustomScoringHelperController controller = new CustomScoringHelperController(model, view);
-		view.setVisible(true);
+		// if no cmd line args, then run GUI version; else, run command line version
+		boolean runGUIVersion = (args.length == 0);
+		if(runGUIVersion) {
+			logger.info("Running GUI version");
+			CustomScoringHelperView view = new CustomScoringHelperView(model);
+			CustomScoringHelperController controller = new CustomScoringHelperController(model, view);
+			view.setVisible(true);
+		} else {
+			logger.info("Running command line version");
+			model.run(args);
+		}
 	}
 }
